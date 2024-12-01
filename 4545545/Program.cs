a01841a2b1d684dcf4f04575318b794be9c75182
USER <USER@LAPTOP-LI2396SB>
2024-12-01 14:05:06 +0800

    加入專案檔案。

---
+++ 4545545/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4545545
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = new int[5];
+        }
+    }
+}
